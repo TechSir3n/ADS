f95806ba6af04cde9975a402455712e4e503d0f7
Ruslan <ruslan56781@mail.ru>
2023-04-29 18:07:33 +0300

    fix code,because it was wrong

--- include/TreeNode.hpp
+++ include/TreeNode.hpp
@@ -4,8 +4,7 @@
 template <typename Type>
 class TreeNode {
 public:
-    explicit TreeNode(Type &&t_val) : 
-        val(std::forward<Type>(t_val)), right(nullptr), left(nullptr) { }
+    explicit TreeNode(const Type &t_val) : val(t_val), right(nullptr), left(nullptr) { }
 
     TreeNode() : val(Type()), right(nullptr), left(nullptr) { }
 
@@ -14,52 +13,8 @@ public:
 
     TreeNode(TreeNode &&t_node) noexcept { *this = std::move(t_node); }
 
-    virtual ~TreeNode() { }
-
-public:
-    void preorderPrint(TreeNode *node) {
-        if (node == nullptr) {
-            return;
-        }
-
-        std::cout << node->val << ' ';
-        preorderPrint(node->left);
-        preorderPrint(node->right);
-    }
-
-    void postorderPrint(TreeNode *node) {
-        if (node == nullptr) {
-            return;
-        }
-
-        postorderPrint(node->left);
-        std::cout << node->val << ' ';
-        postorderPrint(node->right);
-    }
-
-    void reversePrint(TreeNode *node) {
-        if (node == nullptr) {
-            return;
-        }
-
-        reversePrint(node->left);
-        reversePrint(node->right);
-        std::cout << node->val << ' ';
-    }
-
-    int CountNodes(TreeNode *node) {
-        int count = 0;
-        if (node == nullptr) {
-            return 0;
-        } else {
-            count = 1;
-            count += CountNodes(node->left);
-            count += CountNodes(node->right);
-        }
-
-        return count;
-    }
-
+    virtual ~TreeNode() noexcept { }
+    
 protected:
     TreeNode &operator=(TreeNode<Type> &t_node) noexcept {
         if (this != &t_node) {
--- main.cpp
+++ main.cpp
@@ -2,24 +2,10 @@
 #include "./include/BSTree.hpp"
 
 int main(void) {
-    TreeNode<int> *node = new TreeNode<int>(10);
     BSTree<int> bst;
-
-    bst.insert(15, node);
-    bst.insert(23, node);
-    bst.insert(12, node);
-    bst.insert(22, node);
-    bst.erase(15,node);
-    
-    node->preorderPrint(node);
-    std::cout << '\n';
-    
-    // node->postorderPrint(node);
-
-    // std::cout << '\n';
-    // node->reversePrint(node);
-
-    // std::cout<<node->CountNodes(node);
+    bst.insertNode(12);
+    bst.insertNode(19);
+    bst.printElements();
 
     return 0;
 }
